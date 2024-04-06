d6f3ff40bcb2c19c5096f2731cc18b1a139f18ac
kunal-develope <kunalkk45451@gmail.com>
2024-04-06 10:40:57 +0530

    removed video tag

--- src/app/three-60-videos/page.jsx
+++ src/app/three-60-videos/page.jsx
@@ -3,14 +3,6 @@ const three60Videos = () => {
   return (
     <div>
       <Header />
-      <div>
-        <video width="600" height="460" controls>
-          <source
-            src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </div>
     </div>
   );
 };
